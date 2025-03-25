411b66fc53869a3d39aef438b2d3fd788b4cfa05
dlulanep <74797289+dlulanep@users.noreply.github.com>
2025-03-25 19:46:11 +0200

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,10 @@
+numbers = []
+total = int(input("Total Numbers: "))
+if total < 0:
+    print("Invalid id input")
+else:
+    for i in range(1, total+1):
+        number = int(input("number" + str (i) + ":"))
+        numbers.append
+        print(numbers)
+
