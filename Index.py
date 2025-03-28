d94cd926843b73152b0c24510f6b8d94a3fa047f
dlulanep <74797289+dlulanep@users.noreply.github.com>
2025-03-28 23:49:59 +0200

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,10 +1,34 @@
-numbers = []
-total = int(input("Total Numbers: "))
-if total < 0:
-    print("Invalid id input")
-else:
-    for i in range(1, total+1):
-        number = int(input("number" + str (i) + ":"))
-        numbers.append
-        print(numbers)
+#Create an empty my_list
+my_list = []
+#Append 4 elements to my_list
+for i in range(10):
 
+    my_list.append(10*i)
+#Print the my_list
+    print(my_list)
+#Print the length of my_list
+    print(len(my_list))
+#Print the first element of my_list
+    print(my_list[i])
+#Print the last element of my_list
+    print(my_list[i-1])
+#Print the first 4 elements of my_list
+    print(my_list[10:30])
+#Print the last 4 elements of my_list
+    print (my_list[30:40])
+#Extend my_list with 3 elements
+    my_list.extend(10*i for i in range(10))
+#Print extended my_list
+    print(my_list)
+#Insert 15 at index 2 of my_list
+    my_list.insert (2, 15)
+#Sort my_list in asceding order
+    my_list.sort()
+#Print the sorted my_list
+    print(my_list)
+#Print index of 30 in my list
+    print(my_list.index(30))
+
+
+
+    
